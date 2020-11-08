ea9eb496a66a4a86fd961de930795b577cdc8c3a
Sebastian Reitz <sebastianreitz@sebastians-mbp.home>
2020-11-08 18:08:21 +0000

    feat: add discrete distributions

--- conjugates.py
+++ conjugates.py
@@ -3,36 +3,63 @@ import numpy as np
 from distributions import Normal, Gamma, Inv_Gamma
 
 
-CONJUGATE_TABLE = {("Normal", "Normal", "mean"): "normal_normal_mean",
-                   ("Normal", "Gamma", "precision"): "normal_gamma_precision",
-                   ("Normal", "Inv_Gamma", "variance"): "normal_inv_gamma_variance"
-    
-    
-    
-    
-                    }
+CONJUGATE_TABLE = {
+              ("Normal", "Normal", "mean"): "normal_normal_mean",
+              ("Normal", "Gamma", "precision"): "normal_gamma_precision",
+              ("Normal", "Inv_Gamma", "variance"): "normal_inv_gamma_variance",
+              ("Exponential", "Gamma", "rate"): "exponential_gamma_rate",
+              ("Gamma", "Gamma", "rate"): "gamma_gamma_rate",
+              ("Poisson", "Gamma", "rate"): "poisson_gamma_rate"
+              }
 
+CONJUGATES = {("Normal", "Normal"): {"Posterior": "Normal", "On": "Mu"},
+              ("Normal", "Gamma"): {"Posterior": "Gamma", "On": "Tau"},
+              ("Normal", "Inv_Gamma"): {"Posterior": "Inv_Gamma", "On": "Sigma2"},
+              ("Exponential", "Gamma"): {"Posterior": "Gamma", "On": "Lambda"},
+              ("Gamma", "Gamma"): {"Posterior": "Gamma", "On": "Beta"},
+              ("Poisson", "Gamma"): {"Posterior": "Gamma", "On": "Lambda"}
+              }
 
-
+CONJUGATES_INV = {("Normal", "Mu"): "Normal",
+              ("Normal", "Tau"): "Gamma",
+              ("Normal", "Sigma2"): "Inv_Gamma",
+              ("Exponential", "Lambda"): "Gamma",
+              ("Gamma", "Beta"): "Gamma",
+              ("Poisson", "Lambda"): "Gamma"
+              }
 
 
 def get_conjugate(likelihood, prior, parameter: str, observations: list):
     conjugate = CONJUGATE_TABLE[(likelihood.name, prior.name, parameter)]
-    
+
     n = len(observations)
-    
+
     if conjugate == "normal_normal_mean":
-         posterior = Normal(prior.sigma*np.sum(observations)/(likelihood.sigma/n+prior.sigma)
-                            + likelihood.sigma*prior.mu/(likelihood.sigma/n+prior.sigma),
+        posterior = Normal(prior.sigma*np.sum(observations)/
+                            (likelihood.sigma/n+prior.sigma)
+                            + likelihood.sigma*prior.mu/
+                            (likelihood.sigma/n+prior.sigma),
                             1/((1/prior.sigma)+(n/likelihood.sigma)))
 
     elif conjugate == "normal_gamma_precision":
-         posterior = Gamma(prior.alpha + n/2, prior.beta +
-                           sum([(observations[i]-likelihood.mu)**2 for i in range(n)])/2)
-    
+        posterior = Gamma(prior.alpha + n/2, prior.beta +
+                           sum([(observations[i]-likelihood.mu)**2
+                                for i in range(n)])/2)
+
     elif conjugate == "normal_inv_gamma_variance":
-         posterior = Inv_Gamma(prior.alpha + n/2, prior.beta +
-                           sum([(observations[i]-likelihood.mu)**2 for i in range(n)])/2)
+        posterior = Inv_Gamma(prior.alpha + n/2, prior.beta +
+                           sum([(observations[i]-likelihood.mu)**2
+                                for i in range(n)])/2)
+
+    elif conjugate == "exponential_gamma_rate":
+        posterior = Gamma(prior.alpha+n, prior.beta+sum(observations))
+
+    elif conjugate == "gamma_gamma_rate":
+        posterior = Gamma(prior.alpha+n*likelihood.alpha, prior.beta+sum(observations))
+
+    elif conjugate == "poisson_gamma_rate":
+        posterior = Gamma(prior.alpha+sum(observations), prior.beta+n)
+
     return posterior
 
 
--- distributions.py
+++ distributions.py
@@ -1,7 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-from scipy.stats import gamma, norm, invgamma
+from scipy.stats import gamma, norm, invgamma, expon
+from scipy.special import beta as beta_fc
+from scipy.special import gamma as gamma_fc 
+
+
+"""
+Parent class
+"""
 
 
 class Probability_Distribution:
@@ -9,6 +16,7 @@ class Probability_Distribution:
         self.name = name
         self.pdf = None
         self.variance = None
+        self.parameters = None
 
     def print_stuff(self):
         print("TEST")
@@ -22,18 +30,34 @@ class Probability_Distribution:
         y_pdf = [self.pdf(x) for x in domain]
         return plt.plot(domain, y_pdf)
 
+    def plot_cdf(self, x0, xn):
+        domain = np.linspace(x0, xn, 1000)
+        y_cdf= [self.cdf(x) for x in domain]
+        return plt.plot(domain, y_cdf)
 
+    def describe(self):
+        return {"name": self.name, "parameters": self.parameters}
+
+
+"""
+Child classes: Continuous Likelihood
+"""
 class Normal(Probability_Distribution):
     """Initialise Normal distribution."""
     def __init__(self, mu, sigma2):
         self.name = "Normal"
         self.mu = mu
-        self.sigma = sigma2
+        self.sigma2 = sigma2
+        self.parameters = {"mu": self.mu, "sigma2": self.sigma2}
 
     def pdf(self, X):
         pdf = 1/math.sqrt(self.sigma2*math.pi*2)*math.exp(
             -0.5*((X-self.mu)/self.sigma2)**2)
         return pdf
+
+    def cdf(self, X):
+        cdf = norm.cdf(X, loc=self.mu, scale=self.sigma)
+        return cdf
 
     def mean(self):
         mean = self.mu
@@ -42,7 +66,7 @@ class Normal(Probability_Distribution):
     def variance(self):
         var = self.sigma2
         return var
-    
+
     def precision(self):
         precision = 1/self.sigma2
         return precision
@@ -64,14 +88,19 @@ class Gamma(Probability_Distribution):
         self.name = "Gamma"
         self.alpha = alpha
         self.beta = beta
+        self.parameters = {"alpha": self.alpha, "beta": self.beta}
 
     def gamma_fct(self, a):
-        return math.factorial(a-1)
+        return gamma_fc(a)
 
     def pdf(self, X):
-        pdf = (self.beta**self.alpha)/self.gamma_fct(self.alpha)*X**(self.alpha-1)*math.exp(
-            -self.beta*X)
+        pdf = (self.beta**self.alpha)/self.gamma_fct(self.alpha)\
+        *X**(self.alpha-1)*math.exp(-self.beta*X)
         return pdf
+
+    def cdf(self, X):
+        cdf = gamma.cdf(X, self.alpha, scale=1/self.beta)
+        return cdf
 
     def mean(self):
         mean = self.alpha/self.beta
@@ -98,21 +127,27 @@ class Inv_Gamma(Probability_Distribution):
         self.name = "Inv_Gamma"
         self.alpha = alpha
         self.beta = beta
+        self.parameters = {"alpha": self.alpha, "beta": self.beta}
 
     def gamma_fct(self, a):
-        return math.gamma(a)
+        return gamma_fc(a)
 
     def pdf(self, X):
-        pdf = (self.beta**self.alpha)/self.gamma_fct(self.alpha)*X**(-self.alpha-1)*math.exp(
-            -self.beta/X)
+        pdf = (self.beta**self.alpha)/self.gamma_fct(self.alpha)\
+        *X**(-self.alpha-1)*math.exp(-self.beta/X)
         return pdf
+
+    def cdf(self, X):
+        cdf = invgamma.cdf(X, self.alpha, scale=self.beta)
+        return cdf
 
     def mean(self):
         mean = self.beta/(self.alpha-1) if self.alpha > 1 else 9999
         return mean
 
     def variance(self):
-        var = self.beta**2/((self.alpha-1)**2*(self.alpha-2)) if self.alpha > 2 else 9999
+        var = self.beta**2/((self.alpha-1)**2*(self.alpha-2)) if \
+        self.alpha > 2 else 9999
         return var
 
     def equitailed_cs(self, alpha2):
@@ -126,27 +161,71 @@ class Inv_Gamma(Probability_Distribution):
         return (lower_bound, upper_bound)
 
 
+class Exponential(Probability_Distribution):
+    """Initialise Exponential distribution."""
+    def __init__(self, lambda_):
+        self.name = "Exponential"
+        self.lambda_ = lambda_
+        self.parameters = {"lambda": self.lambda_}
+
+    def pdf(self, X):
+        pdf = self.lambda_*math.exp(-self.lambda_*X) if X >=0 else 0
+        return pdf
+
+    def cdf(self, X):
+        pdf = 1-math.exp(-self.lambda_*X) if X >=0 else 0
+        return pdf
+
+    def mean(self):
+        mean = 1/self.lambda_
+        return mean
+
+    def variance(self):
+        var = 1/self.lambda_**2
+        return var
+
+    def equitailed_cs(self, alpha2):
+        """
+        Calculates the equitailed credible set of a parameter.
+        The alpha to be inserted should be between (0-100).
+        """
+        alpha_split = (100-alpha2)/200
+        lower_bound = expon.ppf(alpha_split, scale =1/self.lambda_)
+        upper_bound = expon.ppf(1-alpha_split, scale =1/self.lambda_)
+        return (lower_bound, upper_bound)
 
 
+"""
+Child classes: Discrete Likelihood
+"""
+class Bernoulli(Probability_Distribution):
+    """Initialise Bernoulli distribution."""
+    def __init__(self, p):
+        self.name = "Bernoulli"
+        self.p = p
+        self.parameters = {"probability": self.p}
 
 
+class Beta(Probability_Distribution):
+    """Initialise Beta distribution."""
+    def __init__(self, alpha, beta):
+        self.name = "Beta"
+        self.alpha = alpha
+        self.beta = beta
+        self.parameters = {"alpha": self.alpha, "beta": self.beta}
+
+    def pdf(self, X):
+        pmf = (X**(self.alpha-1)*(1-X)**(self.beta-1))/\
+        beta_fc(self.alpha, self.beta)
+        return pmf
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Poisson(Probability_Distribution):
+    """Initialise Poisson distribution."""
+    def __init__(self, lambda_):
+        self.name = "Poisson"
+        self.lambda_ = lambda_
+        self.parameters = {"lambda": self.lambda_}
 
 
 
