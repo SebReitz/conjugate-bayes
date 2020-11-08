897bb0b6e67b776cd2aa4ccb4dd21ec5194e96cf
Sebastian Reitz <sebastianreitz@sebastians-mbp.home>
2020-11-08 10:03:10 +0000

    feat: add inverse gamma conj

--- conjugates.py
+++ conjugates.py
@@ -1,30 +1,38 @@
 import numpy as np
 
-from distributions import Normal
+from distributions import Normal, Gamma, Inv_Gamma
 
 
-CONJUGATE_TABLE = {("Normal", "Normal", "mu"): "Normal_Normal_mu"
+CONJUGATE_TABLE = {("Normal", "Normal", "mean"): "normal_normal_mean",
+                   ("Normal", "Gamma", "precision"): "normal_gamma_precision",
+                   ("Normal", "Inv_Gamma", "variance"): "normal_inv_gamma_variance"
     
     
     
     
-    }
+                    }
 
 
 
 
 
 def get_conjugate(likelihood, prior, parameter: str, observations: list):
-    lk_kind = likelihood.name
-    prior_kind = prior.name
-    conjugate = CONJUGATE_TABLE[(lk_kind, prior_kind, parameter)]
+    conjugate = CONJUGATE_TABLE[(likelihood.name, prior.name, parameter)]
     
     n = len(observations)
     
-    if conjugate == "Normal_Normal_mu":
+    if conjugate == "normal_normal_mean":
          posterior = Normal(prior.sigma*np.sum(observations)/(likelihood.sigma/n+prior.sigma)
                             + likelihood.sigma*prior.mu/(likelihood.sigma/n+prior.sigma),
                             1/((1/prior.sigma)+(n/likelihood.sigma)))
+
+    elif conjugate == "normal_gamma_precision":
+         posterior = Gamma(prior.alpha + n/2, prior.beta +
+                           sum([(observations[i]-likelihood.mu)**2 for i in range(n)])/2)
+    
+    elif conjugate == "normal_inv_gamma_variance":
+         posterior = Inv_Gamma(prior.alpha + n/2, prior.beta +
+                           sum([(observations[i]-likelihood.mu)**2 for i in range(n)])/2)
     return posterior
 
 
--- distributions.py
+++ distributions.py
@@ -7,9 +7,14 @@ class Probability_Distribution:
     def __init__(self, name):
         self.name = name
         self.pdf = None
+        self.variance = None
 
     def print_stuff(self):
         print("TEST")
+
+    def std(self):
+        std = math.sqrt(self.variance())
+        return std
 
     def plot_pdf(self, x0, xn):
         domain = np.linspace(x0, xn, 1000)
@@ -20,15 +25,28 @@ class Probability_Distribution:
 
 class Normal(Probability_Distribution):
     """Initialise Normal distribution."""
-    def __init__(self, mu, sigma):
+    def __init__(self, mu, sigma2):
         self.name = "Normal"
         self.mu = mu
-        self.sigma = sigma
+        self.sigma = sigma2
 
     def pdf(self, X):
-        pdf = 1/math.sqrt(self.sigma*math.pi*2)*math.exp(
-            -0.5*((X-self.mu)/self.sigma)**2)
+        pdf = 1/math.sqrt(self.sigma2*math.pi*2)*math.exp(
+            -0.5*((X-self.mu)/self.sigma2)**2)
         return pdf
+
+    def mean(self):
+        mean = self.mu
+        return mean
+
+    def variance(self):
+        var = self.sigma2
+        return var
+    
+    def precision(self):
+        precision = 1/self.sigma2
+        return precision
+
 
 
 class Gamma(Probability_Distribution):
@@ -46,8 +64,37 @@ class Gamma(Probability_Distribution):
             -self.beta*X)
         return pdf
 
+    def mean(self):
+        mean = self.alpha/self.beta
+        return mean
+    
+    def variance(self):
+        var = self.alpha/self.beta**2
+        return var
 
 
+class Inv_Gamma(Probability_Distribution):
+    """Initialise Inverse Gamma distribution."""
+    def __init__(self, alpha, beta):
+        self.name = "Inv_Gamma"
+        self.alpha = alpha
+        self.beta = beta
+
+    def gamma_fct(self, a):
+        return math.gamma(a)
+
+    def pdf(self, X):
+        pdf = (self.beta**self.alpha)/self.gamma_fct(self.alpha)*X**(-self.alpha-1)*math.exp(
+            -self.beta/X)
+        return pdf
+
+    def mean(self):
+        mean = self.beta/(self.alpha-1) if self.alpha > 1 else 9999
+        return mean
+
+    def variance(self):
+        var = self.beta**2/((self.alpha-1)**2*(self.alpha-2)) if self.alpha > 2 else 9999
+        return var
 
 
 
